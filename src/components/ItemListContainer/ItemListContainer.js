dd48b7373cf8ad26081ec3381537cb14489cb711
facundo beretta <facuberetta.fb@gmail.com>
2025-01-27 12:11:36 -0300

    react preentrega 2

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react"
+import {getProducts} from '../../asyncMock'
+import itemList from '../itemList/itemList'
+
+
 const ItemListContainer = ({greeting}) => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        getProducts()
+            .then(response => {
+                setProducts(response)
+            })
+            .catch(error => {
+                console.error(error)
+        }, [])
+
+
     return (
         <div>
             <h1>{greeting}</h1>
         </div>
     )
 }
-export default ItemListContainer 
+)}
+
+
+export default ItemListContainer
